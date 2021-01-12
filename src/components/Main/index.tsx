6cdcb7280568ebaebb72630722545f957ba4d20e
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-01-12 12:07:34 -0300

    feat(storybook): Make initial config and initial stories for Main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,15 +1,16 @@
 import * as Style from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components',
+}) => (
   <Style.Wrapper>
     <Style.Logo
       src="/img/logo.svg"
       alt="Imagem de um átomo e React Avançado escrito ao lado."
     />
-    <Style.Title>React Avançado</Style.Title>
-    <Style.Description>
-      TypeScript, ReactJS, NextJS e Styled Components
-    </Style.Description>
+    <Style.Title>{title}</Style.Title>
+    <Style.Description>{description}</Style.Description>
     <Style.Illustration
       src="/img/hero-illustration.svg"
       alt="Um desenvolvedor de frente para uma tela com código."
