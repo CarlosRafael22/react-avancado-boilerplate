8bfaaed9dd05cf23fc97a2396af0b7ed193e576f
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-01-12 10:21:25 -0300

    feat(main): Create styled components and change Main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as Style from './styles'
+
 const Main = () => (
-  <div>
-    <h2>Testing Main</h2>
-  </div>
+  <Style.Wrapper>
+    <Style.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <Style.Title>React Avançado</Style.Title>
+    <Style.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </Style.Description>
+    <Style.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código."
+    />
+  </Style.Wrapper>
 )
 
 export default Main
