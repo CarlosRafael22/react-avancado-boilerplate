08c9047958c5102093acfd36d41017c9dabf4242
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-01-12 09:54:59 -0300

    feat: Create _document and _app for Next.js with Main component

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import '../styles/globals.css'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title> Next.js React app - Boilerplate </title>
+        <meta
+          name="description"
+          content="A simple boilerplate to have a NextJS project with React, TypeScript and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
